2a06dcd496b9f1e056b3dce940dbb1f3258ba4f0
unknown <amir98sajadian@gmail.com>
2025-11-20 10:19:52 +0330

    commit1

---
+++ home_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render 
+from django.views.generic import TemplateView 
+
+class Home(TemplateView): 
+    template_name = 'home_app/index.html' 
+# Create your views here.
